10c9c5e9cc11e36aafa1c437402f8bd41a924b52
roipaname <roipaname@gmail.com>
2025-08-11 19:42:15 +0200

    setting up

--- src/dataloader/image_loader.py
+++ src/dataloader/image_loader.py
@@ -3,3 +3,7 @@ import tensorflow as tf
 
 data_dir="data/phishingimage"
 image_ext=['jpeg','png','jpg','bmg']
+print(os.listdir(data_dir))
+for img_class in os.listdir(data_dir):
+    for image in os.listdir(os.path.join(data_dir,img_class)):
+        print(image)
